1f49a2cde525ee9de24f128a62bffbde9ed5c4c1
Senujide <senujidecosta@gmail.com>
2026-03-08 21:18:35 +0530

    Modified the calculator by adding modulud and power operations

--- python_cal.py
+++ python_cal.py
@@ -5,3 +5,7 @@ print("Addition =", x+y)
 print("Subtraction =", x-y)
 print("Multiplication =", x*y)
 print("Division =", x/y)
+
+#added modulus and power operations
+print("Modulus =", x % y)
+print("Power =", x ** y)
